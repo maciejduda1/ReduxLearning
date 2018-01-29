c8ad439f40dc570155e0298528e3b3d576e02aef
Maciej Duda <maciej.duda1@gazeta.pl>
2018-01-29 17:12:20 +0100

    Edition added

--- src/Comment.js
+++ src/Comment.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React, {Component} from 'react';
+//import ReactDOM from 'react-dom';
+/*
 const Comment = ({text, votes, id, thumbUpComment, thumbDownComment, removeComment, editComment}) =>
   <li className="comment">
+		<CommentText />
     {text} <span className="votesSection"> votes: {votes}</span>
     <div id = {id}>
     	<button onClick={() => removeComment(id)}>Remove</button> 
@@ -11,6 +12,55 @@ const Comment = ({text, votes, id, thumbUpComment, thumbDownComment, removeComme
     	<button onClick={() => thumbDownComment(id)}> Thumb Down </button> 
   	</div>
   </li>;
+*/
 
+class Comment extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			inEdition: 'no'
+		}
+	}
+
+	handleEdition(e) {
+		e.preventDefault();
+		this.setState({inEdition: 'yes'});
+	}
+
+	render() {
+		if (this.state.inEdition === 'no'){
+			return (
+				<li className="comment">
+    			{this.props.text} <span className="votesSection"> votes: {this.props.votes}</span>
+    			<div id = {this.props.id}>
+    				<button onClick={() => this.props.removeComment(this.props.id)}>Remove</button> 
+    				<button onClick={(e) => this.handleEdition(e)}>Edycja</button>  
+    				<button onClick={() => this.props.thumbUpComment(this.props.id)}>Thumb up</button> 
+    				<button onClick={() => this.props.thumbDownComment(this.props.id)}> Thumb Down </button> 
+  				</div>
+				</li>
+			);
+		} else if (this.state.inEdition === 'yes') {
+			
+			return (
+				<li className="comment">
+					<form onSubmit={(e) => {
+							e.preventDefault(); 
+							this.props.editComment(e.target.commentText.value, this.props.id);
+							 this.setState({inEdition: 'no'});
+						}}>
+				  	<input
+						type = "text"
+						name = "commentText"
+						defaultValue = {this.props.text}
+						/>
+						<input type="submit" value="Submit" />
+					</form>
+				</li>
+			);
+		}
+	}
+
+}
 export default Comment;
 
